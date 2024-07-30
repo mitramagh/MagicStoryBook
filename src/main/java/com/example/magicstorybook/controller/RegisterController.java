b77ef7a075febfbb3ea3db1463139a809073511c
Mitra <mit.magh@gmail.com>
2024-07-29 20:52:29 -0400

    added /register to routes

--- src/main/java/com/example/magicstorybook/controller/RegisterController.java
+++ src/main/java/com/example/magicstorybook/controller/RegisterController.java
@@ -1,15 +1,10 @@
 package com.example.magicstorybook.controller;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
 import com.example.magicstorybook.model.User;
 import com.example.magicstorybook.repository.UserRepository;
 
@@ -42,4 +37,3 @@ public class RegisterController {
         return ResponseEntity.ok(newUser);
     }
 }
-
